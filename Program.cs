33c47ce3c5a425ae1fb5aec57fe3b4391d6846e7
Kandr9 <kandr13579@gmail.com>
2023-06-26 07:03:02 +0300

    задача 4

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите целое число N");
+int numN = Convert.ToInt32(Console.ReadLine());
+int count=-numN;
+
+while (count<=numN)
+{   System.Console.Write(count+", ");
+    count++;
+    }
