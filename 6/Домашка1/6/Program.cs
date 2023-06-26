573d877af4dded02652d14665da5e003a4b64370
Kandr9 <kandr13579@gmail.com>
2023-06-26 07:31:58 +0300

    задача 6

---
+++ 6/Домашка1/6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Проверка числа на чётность без остатка.
+
+Console.WriteLine("Введите  число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num%2==0)
+{System.Console.WriteLine(num+" - чётное число");}
+else 
+System.Console.WriteLine("нечётное число");
